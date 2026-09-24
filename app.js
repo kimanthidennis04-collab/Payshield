2e9cb1c3096948514d0e02cc5ff0766aebe39c89
kimanthidennis04-collab <kimanthidennis04@gmail.com>
2026-09-25 01:46:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Startup check: Ensure default Organization and a default Client exist
 async function ensureDefaultData() {
